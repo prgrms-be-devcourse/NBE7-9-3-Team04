8500e839fc2b12bd2719a990c81731c75a0aeca1
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-11-13 14:33:50 +0900

    refactor: JwtAuthenticationFilter 전환

--- backend/src/main/java/com/backend/global/security/CustomUserDetailsService.kt
+++ backend/src/main/java/com/backend/global/security/CustomUserDetailsService.kt
@@ -3,14 +3,12 @@ package com.backend.global.security
 import com.backend.domain.user.repository.UserRepository
 import com.backend.global.exception.ErrorCode
 import com.backend.global.exception.ErrorException
-import lombok.RequiredArgsConstructor
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 //DB에서 사용자 정보 로드
 @Service
-@RequiredArgsConstructor
 class CustomUserDetailsService(
     private val userRepository: UserRepository
 ) : UserDetailsService {
