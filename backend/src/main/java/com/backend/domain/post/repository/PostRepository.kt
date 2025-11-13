302bbef89c5449718d9470666cae23ac01728764
김규섭 <ryan12256@naver.com>
2025-11-13 17:49:09 +0900

    refactor :  post 레포지토리 수정

--- backend/src/main/java/com/backend/domain/post/repository/PostRepository.kt
+++ backend/src/main/java/com/backend/domain/post/repository/PostRepository.kt
@@ -1,29 +1,27 @@
-package com.backend.domain.post.repository;
+package com.backend.domain.post.repository
 
-import com.backend.domain.post.entity.PinStatus;
-import com.backend.domain.post.entity.Post;
-import com.backend.domain.post.entity.PostCategoryType;
-import com.backend.domain.post.entity.PostStatus;
-import com.backend.domain.user.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.backend.domain.post.entity.PinStatus
+import com.backend.domain.post.entity.Post
+import com.backend.domain.post.entity.PostCategoryType
+import com.backend.domain.post.entity.PostStatus
+import com.backend.domain.user.entity.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
+import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
+import java.util.*
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+interface PostRepository : JpaRepository<Post, Long> {
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+    @EntityGraph(attributePaths = ["users"])
+    fun findByPostCategoryType(categoryType: PostCategoryType, pageable: Pageable): Page<Post>
 
-    @EntityGraph(attributePaths = {"users"})
-    Page<Post> findByPostCategoryType(PostCategoryType categoryType, Pageable pageable);
+    @EntityGraph(attributePaths = ["users"])
+    fun findByUsers(user: User, pageable: Pageable): Page<Post>
 
-    @EntityGraph(attributePaths = {"users"})
-    Page<Post> findByUsers(User user, Pageable pageable);
+    @EntityGraph(attributePaths = ["users"])
+    fun findByPinStatusAndStatusOrderByCreateDateDesc(pinStatus: PinStatus, status: PostStatus): List<Post>
 
-    @EntityGraph(attributePaths = {"users"})
-    List<Post> findByPinStatusAndStatusOrderByCreateDateDesc(PinStatus pinStatus, PostStatus status);
-
-    Optional<List<Post>> findByStatusAndDeadlineLessThan(PostStatus status, LocalDateTime now);
+    fun findByStatusAndDeadlineLessThan(status: PostStatus, now: LocalDateTime): Optional<List<Post>>
 }
