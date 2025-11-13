95b29cb263ee48302eab0e780ecc81e450b4ec6a
김규섭 <ryan12256@naver.com>
2025-11-13 14:34:41 +0900

    refactor : SchedulerConfig 변환

--- backend/src/main/java/com/backend/global/config/SchedulerConfig.kt
+++ backend/src/main/java/com/backend/global/config/SchedulerConfig.kt
@@ -1,20 +1,18 @@
-package com.backend.global.config;
+package com.backend.global.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
-public class SchedulerConfig{
-
+class SchedulerConfig {
     @Bean
-    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(Runtime.getRuntime().availableProcessors() * 2); //cpu 코어 개수 2배로 설정
-        scheduler.initialize();
-        return scheduler;
-    }
+    fun threadPoolTaskScheduler(): ThreadPoolTaskScheduler =
+        ThreadPoolTaskScheduler().apply {
+            poolSize = Runtime.getRuntime().availableProcessors() * 2 //cpu 코어 개수 2배로 설정
+            initialize()
+        }
 }
