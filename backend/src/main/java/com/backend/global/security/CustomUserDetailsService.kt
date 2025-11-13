e1fca526a14a5fc7f6854e900e6ad005863c4cf0
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-11-13 14:25:54 +0900

    refactor: CustomUserDetailsService 전환

---
+++ backend/src/main/java/com/backend/global/security/CustomUserDetailsService.kt
@@ -0,0 +1,24 @@
+package com.backend.global.security
+
+import com.backend.domain.user.repository.UserRepository
+import com.backend.global.exception.ErrorCode
+import com.backend.global.exception.ErrorException
+import lombok.RequiredArgsConstructor
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+//DB에서 사용자 정보 로드
+@Service
+@RequiredArgsConstructor
+class CustomUserDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(email: String): UserDetails {
+        val user = userRepository.findByEmail(email)
+            .orElseThrow { ErrorException(ErrorCode.NOT_FOUND_EMAIL) }
+
+        return CustomUserDetails(user)
+    }
+}
