25a87cecfb4a78741c12c2f1f6cbc9932c3d0d77
Labtory-82 <chnny9@gmail.com>
2025-11-17 09:35:04 +0900

    refactor: 롬복 제거#21

--- backend/src/main/java/com/backend/api/comment/controller/CommentController.kt
+++ backend/src/main/java/com/backend/api/comment/controller/CommentController.kt
@@ -10,11 +10,9 @@ import com.backend.global.dto.response.ApiResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
-import lombok.RequiredArgsConstructor
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
 @Tag(name = "CommentController", description = "댓글 API")
 class CommentController(
