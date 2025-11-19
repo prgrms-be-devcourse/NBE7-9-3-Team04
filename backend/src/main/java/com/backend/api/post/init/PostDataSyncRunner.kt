b6cf741d21b5b17ddebf41e7c2de02891beb57d8
larama-C <ccbj0825@naver.com>
2025-11-19 10:30:03 +0900

    fix: 테스트 프로필 비활성화 추가

--- backend/src/main/java/com/backend/api/post/init/PostDataSyncRunner.kt
+++ backend/src/main/java/com/backend/api/post/init/PostDataSyncRunner.kt
@@ -5,11 +5,13 @@ import com.backend.domain.post.repository.PostRepository
 import com.backend.domain.post.repository.search.PostSearchRepository
 import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import javax.annotation.PostConstruct
 
+@Profile("!test")
 @Component
 class PostDataSyncRunner(
     private val postRepository: PostRepository,
--- backend/src/main/java/com/backend/api/user/init/UserDataSyncRunner.kt
+++ backend/src/main/java/com/backend/api/user/init/UserDataSyncRunner.kt
@@ -5,10 +5,12 @@ import com.backend.domain.user.repository.UserRepository
 import com.backend.domain.user.repository.search.UserSearchRepository
 import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
+@Profile("!test")
 @Component
 class UserDataSyncRunner(
     private val userRepository: UserRepository,
