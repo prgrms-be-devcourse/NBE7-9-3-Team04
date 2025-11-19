b22724b050dc56e571303da94504cf58dc155b28
Labtory <chnny9@gmail.com>
2025-11-19 11:00:32 +0900

    충돌 해결#main

--- backend/src/main/java/com/backend/api/user/dto/response/SolvdeProblemResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.backend.api.user.dto.response
-
-import java.time.LocalDateTime
-
-data class SolvedProblemResponse (
-    val title: String,// 문제 제목
-    val modifyDate: LocalDateTime?// 수정일
-)
